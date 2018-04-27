4438aa98035398c93e9cfd75f96cec0632bde8a5
bohdanhrytsenko <hrytsenkobohdan@gmail.com>
2018-04-27 09:59:17 +0200

    new portfolio

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,35 +1,70 @@
 jQuery( document ).ready(function() {
 
-    // Portfolio
+    // Scroll to
 
-    $('.portfolio_wrap a[href^="#"]').on('click', function(event) {
-        $('body').addClass('body_bg');
-        var target = $( $(this).attr('href') );
-        target.fadeToggle(100);
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 500);
-        }cd
+    $(".icon").on("click", function(e) {
+        e.preventDefault();
+        var o = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(o).offset().top
+        }, 1000);
+    });
+
+    // Add class menu
+
+    $('.header_nav a').on('click', function () {
+        $('body').toggleClass('overflow');
+        $('.header_menu').toggleClass('header_menu__active');
+        $('.nav_item').toggleClass('nav_item__active');
+    });
+
+
+
+    // Add class on scroll
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".header_menu__item").addClass("header_menu__scroll");
+        } else
+            $(".header_menu__item").removeClass("header_menu__scroll");
+    });
+
+    // Skills
+
+    $(".info_more__text").fadeOut(500);
+    $(".btn_more").on("click", function(e) {
+
+        $(this).toggleClass("btn_close");
+
+        if ($(this).text() == "Close")
+            $(this).text("More")
+        else
+            $(this).text("Close");
+        var o = $($(this).attr("href"));
+        o.fadeToggle(500), o.length && (e.preventDefault(), $("html, body").animate({
+
+        }, 800)),
+            $(".info_more__text" + o).toggle(1e3)
+    });
+    $(".btn_close").on("click", function(e) {
+        $(".info_more__text").fadeOut(500);
 
     });
 
-    $( ".portfolio_close" ).click(function() {
-        $( ".portfolio_full" ).slideUp(500);
-        $('body').removeClass('body_bg');
-    });
 
     // Scroll magic
 
     var controller = new ScrollMagic.Controller();
 
     // Header name
-
+    var t0 = new TimelineLite();
+    t0.staggerFromTo('.header_name span', 4, { opacity: 0}, { opacity: 1}, 0.2);
     var t1 = new TimelineLite();
-    t1.staggerFromTo('.header_name span', 4, { opacity: 0}, { opacity: 1}, 0.2);
+    t1.staggerFromTo('.header_name span:first-child', 2, { rotation: 180}, { rotation: 0}, 0.2);
     var t2 = new TimelineLite();
     t2.staggerFromTo('.header__title .title', 8, { opacity: 0}, { opacity: 1}, 0.2);
+
 
     // Preloader
     (function() {
